770733131addb51aaeed028fa19c426760018b63
prashant_rathour <prathour884@gmail.com>
2023-06-08 00:01:59 +0530

    mongodb

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const connection = require("./db")
 const userrouter = require("./router/user.router")
 const noterouter = require("./router/note.router")
 const app = express()
+const cors=require("cors")
 app.use(express.json())
 
+app.use(cors())
 app.use("/user",userrouter)
 app.use("/notes",noterouter)
-
 app.listen(process.env.PORT,async()=>{
     try {
         await connection
